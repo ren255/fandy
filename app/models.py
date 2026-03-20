1faa626207a0b444e1e84abe8fdd6f923c25be40
ren255 <renpetertoda@gmail.com>
2026-03-20 14:33:42 +0900

    model

--- app/models.py
+++ app/models.py
@@ -2,23 +2,45 @@ from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
+class User(SQLModel, table=True):
+    """ユーザー"""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(max_length=140)
+    password: str
+
+
+class Event(SQLModel, table=True):
+    """イベント"""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_by: int = Field(foreign_key="user.id")
+    created_timestamp: int
+    description: str
+
+
 class UploadedFile(SQLModel, table=True):
-    """files/ に保存したファイルのメタデータ。"""
+    """files/ に保存したファイルのメタデータ"""
 
     id: Optional[int] = Field(default=None, primary_key=True)
     created_timestamp: int
-    username: str = Field(max_length=140)
+    created_by: int = Field(foreign_key="user.id")
     original_name: str
-    file_path: str
     mime_type: str
+    file_path: str
 
 
-class Note(SQLModel, table=True):
-    """140文字のMarkdownノート。file_idで添付ファイルを1つ持てる。"""
+class Photo(SQLModel, table=True):
+    """イベントに紐づく写真"""
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    created_timestamp: int
-    updated_timestamp: int
-    username: str = Field(max_length=140)
-    body: str = Field(max_length=140)
-    file_id: Optional[int] = Field(default=None, foreign_key="uploadedfile.id")
+    file_id: int = Field(foreign_key="uploadedfile.id")
+    event_id: int = Field(foreign_key="event.id")
+
+
+class JoinedEvent(SQLModel, table=True):
+    """ユーザーが参加するイベント。"""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    event_id: int = Field(foreign_key="event.id")
